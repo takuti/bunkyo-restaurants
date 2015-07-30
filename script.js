b57bb1e05f64e4319c40fe5648b4ad80617a9ffc
takuti <k.takuti@gmail.com>
2015-07-30 21:44:29 +0900

    update zoom feature

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // set leaftlet map
-var map = L.map('map', { zoomControl:false }).setView([35.72, 139.75], 14);
+var map = L.map('map').setView([35.72, 139.75], 14);
 L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    minZoom: 14,
-    maxZoom: 14,
+    minZoom: 11,
+    maxZoom: 15,
     id: 'takuti.5bebb9b7',
     accessToken: 'pk.eyJ1IjoidGFrdXRpIiwiYSI6ImFmM2UxMmFjMGI5MDA2NjIzYmZlODJkYzBkYThkZjAxIn0.VuS-dUlX3RIldJBJstFLMw'
 }).addTo(map);
